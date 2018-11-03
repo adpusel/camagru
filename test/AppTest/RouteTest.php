79788875ad7debd04e5260f322d47bffd1d63aa4
get <adrien.pusel@gmail.com>
2018-11-03 08:55:04 +0100

    test route --> ajout de tes sur le res de match

--- test/AppTest/RouteTest.php
+++ test/AppTest/RouteTest.php
@@ -9,8 +9,17 @@ namespace Core\Router;
 
 use PHPUnit\Framework\TestCase;
 
+
+/**
+ * Class RouterTest
+ *
+ * @package Core\Router
+ */
 class RouteTest extends TestCase
 {
+  /**
+   * test the route object
+   */
   public function testConstruct()
   {
 	$xml = new \DOMDocument;
@@ -30,22 +39,53 @@ class RouteTest extends TestCase
 	$this->assertEquals($oneVarRoute->hasVars(), true);
 	$this->assertEquals($oneVarRoute->getTabVars(), ['id']);
 
-	// je fais le match
-	$oneVarRoute->match('oneVar-42');
-	$this->assertEquals($oneVarRoute->getTabVars(), ['id' => 42]);
-
-
 	// test twoVar
 	$twoVarRoute = new Route($routes_xml_tab[2]);
 	$this->assertEquals($twoVarRoute->hasVars(), true);
 	$this->assertEquals($twoVarRoute->getTabVars(), ['id', 'name']);
 
-	// je fais le match
+	return compact('noVarRoute',
+	  'oneVarRoute', 'twoVarRoute');
+  }
+
+  /**
+   *
+   * @var $routeTab array create pre function
+   * @depends testConstruct
+   */
+  public function testMatch(array $routeTab)
+  {
+	extract($routeTab);
+
+	// No
+	/** @noinspection PhpUndefinedVariableInspection */
+
+	$res = $noVarRoute->match('noVar');
+	$this->assertEquals(1, $res);
+
+	$res = $noVarRoute->match('noVaraoeu');
+	$this->assertEquals(false, $res);
+
+	// je fais le One
+	/** @noinspection PhpUndefinedVariableInspection */
+
+	$res = $oneVarRoute->match('oneVar-42');
+	$this->assertEquals(1, $res);
+	$this->assertEquals($oneVarRoute->getTabVars(), ['id' => 42]);
+
+	// je fais le Two
+	/** @noinspection PhpUndefinedVariableInspection */
+
 	$res = $twoVarRoute->match('twoVar-42-super');
 	$this->assertEquals(1, $res);
 	$this->assertEquals(['id' => 42, 'name' => 'super'],
-
+	  $twoVarRoute->getTabVars()
 	);
-
   }
+
+
+//  public function testRouter( )
+//  {
+//
+//  }
 }
